82f9ffb91043866e47d38f959957072e35afda96
fredbanda <fredcbanda@hotmail.com>
2023-09-16 23:08:48 +0200

    added user model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,25 +2,31 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Ruby on Rails CRUD App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails CRUD App"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About Us | Ruby on Rails CRUD App"
   end
 
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact Us | Ruby on Rails CRUD App"
+  end
+
   test "should get privacy_policy" do
-    get static_pages_privacy_policy_url
+    get privacy_policy_path
     assert_response :success
     assert_select "title", "Privacy Policy | Ruby on Rails CRUD App" 
   end
