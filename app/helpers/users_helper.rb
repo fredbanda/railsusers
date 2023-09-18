e26ccf664dafd862e8bda9b08982dc4dcc6d03e4
fredbanda <fredcbanda@hotmail.com>
2023-09-18 16:15:20 +0200

    Working sign up form finished

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+    #Returns the Gravatar for the given user.
+    def gravatar_for(user )
+        gravata_id = Digest::MD5::hexdigest(user.email).downcase
+        gravat_url = "https://www.gravatar.com/avatar/00000000000000000000000000000000"
+        image_tag(gravat_url, alt: user.name, class: "gravatar rounded-circle")
+    end
 end
